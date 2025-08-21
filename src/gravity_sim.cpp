e72b66f8035ebe5b5d96922014c3343e28b5c88d
arifkm2006 <arifkmofficial@gmail.com>
2025-08-21 19:35:02 +0530

    simple window in OpenGL

--- src/gravity_sim.cpp
+++ src/gravity_sim.cpp
@@ -1,1 +1,25 @@
 #include<bits/stdc++.h>
+#include<GLFW/glfw3.h>
+GLFWwindow* window_int(){
+    if(!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return nullptr;
+    }
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Gravity Simulation", nullptr, nullptr);
+    return window;
+};
+int main(){
+    GLFWwindow* window = window_int();
+    if (!window) {
+        return -1;
+    }
+    while (!glfwWindowShouldClose(window)) {
+        // Render your simulation here
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
