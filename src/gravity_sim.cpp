628ab2cf29caff5b85c5baebb593ad9b04dcfa7b
arifkm2006 <arifkmofficial@gmail.com>
2025-08-21 20:54:17 +0530

    simple GLF window

--- src/gravity_sim.cpp
+++ src/gravity_sim.cpp
@@ -1,25 +1,41 @@
-#include<bits/stdc++.h>
+#include<GL/glew.h>
 #include<GLFW/glfw3.h>
-GLFWwindow* window_int(){
-    if(!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return nullptr;
-    }
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Gravity Simulation", nullptr, nullptr);
-    return window;
-};
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<bits/stdc++.h>
+
+GLFWwindow* StartGLFW();
 int main(){
-    GLFWwindow* window = window_int();
+    GLFWwindow* window = StartGLFW();
     if (!window) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
+
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Render your simulation here
+
+        // Render your scene here
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+}
+GLFWwindow* StartGLFW() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return nullptr;
+    }
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Gravity Simulation", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+    return window;
 }
